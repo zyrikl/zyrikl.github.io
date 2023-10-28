f9cca2a856996ca8ee610e8f817b5884fe29f70e
omega-beta <140123838+omega-beta@users.noreply.github.com>
2023-10-27 21:38:29 -0400

    Update addPosts.php

--- addPosts.php
+++ addPosts.php
@@ -2,7 +2,12 @@
 $conn = mysql_connect("localhost", "joeuser", "somepass")
   or die(mysql_error());
 mysql_select_db("testDB", $conn) or die(mysql_error());
-$verify_topic = "select 
+$verify_topic = "select textOfPost from forum_topics where
+  topic_id = $_GET[topic_id]";
+$verify_topic_res = mysql_query($verifty_topic, $conn)
+  or die(mysql_error());
+$topic_title = stripslashes(mysql_result($verify_topic_res,0,"textOfPost"));
+?>
 
 <html>
   <head>
