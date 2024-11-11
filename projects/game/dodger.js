d147ccc50609b241114bd2a5021abca8787d0da8
Charles Wang <140123838+vespulavulgaris@users.noreply.github.com>
2024-11-11 10:22:43 -0500

    tteeet

--- projects/game/dodger.js
+++ projects/game/dodger.js
@@ -2,7 +2,10 @@ var programCode = function(processingInstance) {
     with (processingInstance) {
         size(400, 400); 
         frameRate(60);
+        fill(255, 255, 255);
         rect(0, 0, 400, 400);
+        fill(255, 0, 0);
+        text("Play", 0, 0);
     }
 };
 var canvas = document.getElementById("canvas2");
